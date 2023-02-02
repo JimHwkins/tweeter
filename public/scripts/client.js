351cce16b074d99623b298e3560f5e8f7b8d08ef
JimHwkins <howlarrior@gmail.com>
2023-02-02 16:18:00 -0300

    Add display for validation errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,12 +4,14 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(() => {
+  // Function to prevent XSS
   const escape = (str) => {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  // Function to render tweet with html
   const createTweetElement = (tweet) => {
     const $tweet = $(`
   <article class="tweet-container">
@@ -41,13 +43,18 @@ $(document).ready(() => {
     return $tweet;
   };
 
+  // Function to retrieve tweet from tweets
   const renderTweets = (tweets) => {
+    // Empties the tweets feed before rendering
+    $(".tweets-feed-container").empty();
     for (const tweet of tweets) {
       const newTweet = createTweetElement(tweet);
+      // Puts new tweet on the top of the feed
       $(".tweets-feed-container").prepend(newTweet);
     }
   };
 
+  // Function to load tweets with ajax
   const loadTweets = () => {
     $.ajax({
       method: "GET",
@@ -57,23 +64,42 @@ $(document).ready(() => {
     });
   };
 
+  // Tweet submission process
   const $form = $("#tweet-form");
   $form.on("submit", (event) => {
+    // Prevents refresh from form submit
     event.preventDefault();
     const tweetText = $("#tweet-text").val();
-    if (tweetText.length > 140) {
-      return alert("The tweet is limited to 140 characters only");
+    $(".error-message").hide();
+    // Initial conditionals for tweet posting
+    if (tweetText.length > 10) {
+      const $error = $("<div>")
+        .text("Your message is limited to 140 characters only")
+        .addClass("error-message");
+      $("#tweet-form").prepend($error);
+      return;
     }
-    if (tweetText === "" || tweetText === null) {
-      return alert("The tweet can't be empty");
+    if (!tweetText) {
+      const $error = $("<div>")
+        .text("The tweet cannot be empty")
+        .addClass("error-message");
+      $("#tweet-form").prepend($error);
+      return;
     }
+
     const serializedData = $form.serialize();
     $.ajax({
       method: "POST",
       url: "/tweets",
       data: serializedData,
-    }).then(serializedData);
+    })
+      .then(loadTweets())
+      .then(serializedData);
     loadTweets();
+    // Restarts textarea and character counter
+    $("#tweet-text").val("");
+    $(".counter").text(140);
   });
+
   loadTweets();
 });
