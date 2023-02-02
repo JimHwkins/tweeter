9c7130074c36b303e7f3e1aa6d4eaf5862236222
JimHwkins <howlarrior@gmail.com>
2023-02-01 22:40:44 -0300

    Add form submission using JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,23 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1675121885410,
-  },
-];
-
-const createTweetElement = (tweet) => {
-  const $tweet = $(`
+$(document).ready(() => {
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`
   <article class="tweet-container">
   <div class="user">
     <img
@@ -37,7 +23,7 @@ const createTweetElement = (tweet) => {
   </div>
   <div class="line"></div>
   <footer class="tweet">
-    <div class="data">${timeago.format(tweet.user.created_at)}</div>
+    <div class="data">${timeago.format(tweet.created_at)}</div>
     <div class="icon-container">
       <i class="fas fa-flag"></i>
       <i class="fas fa-retweet"></i>
@@ -46,17 +32,27 @@ const createTweetElement = (tweet) => {
   </footer>
 </article>`);
 
-  return $tweet;
-};
+    return $tweet;
+  };
 
-const renderTweets = (tweets) => {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  for (const tweet of tweets) {
-    const newTweet = createTweetElement(tweet);
-    $("#tweets-container").append(newTweet);
-  }
-};
+  const renderTweets = (tweets) => {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+    for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $("#tweets-container").append(newTweet);
+    }
+  };
 
-renderTweets(data);
+  const $form = $("#tweet-form");
+  $form.on("submit", (event) => {
+    event.preventDefault();
+    const serializedData = $form.serialize();
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: serializedData,
+    }).then(serializedData);
+  });
+});
